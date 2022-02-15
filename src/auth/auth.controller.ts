fcc56d70383fa99db8cec1e11cf880578807afb9
abrambagus <you@example.com>
2022-02-15 15:22:34 +0700

    register-with-validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import * as bcrypt from 'bcryptjs';
+import { RegisterDto } from './models/register.dto';
 
 @Controller()
 export class AuthController {
+  constructor(private userService: UserService) {}
+
   @Post('register')
-  async register(@Body() body) {
-    return body;
+  async register(@Body() body: RegisterDto) {
+    if (body.password !== body.password_confirm) {
+      throw new BadRequestException('Password do not match');
+    }
+
+    const hashed = await bcrypt.hash(body.password, 12);
+    return this.userService.create({
+      email: body.email,
+      password: hashed,
+    });
   }
 }
